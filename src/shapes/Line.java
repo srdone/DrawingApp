cb98a8f844ca9bd0c8e3036bf37a0f412168aefd
Stephen Done <srdone@gmail.com>
2013-02-28 21:34:09 -0700

    Fix to line "containsShape"
    The contains shape for line was overridden with a null method. That was
    removed to allow the line to be selected.

--- src/shapes/Line.java
+++ src/shapes/Line.java
@@ -12,12 +12,6 @@ public class Line extends OpenShape {
     g.drawLine(getX1(), getY1(), getX2(), getY2());
     g.setColor(oldColor);
   }
-
-  @Override
-  public boolean containsLocation(int x, int y) {
-    // TODO Auto-generated method stub
-    return false;
-  }
   
   public String toString() {
     return "Line: \n\tx1 = " + getX1() + "\n\ty1 = " + getY1() + 
